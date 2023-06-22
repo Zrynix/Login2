6c42263656625562050186f4ae8ef14538b58ee1
Amine Aouragh <112860666+Zrynix@users.noreply.github.com>
2023-06-22 11:36:36 +0200

    Add files via upload

---
+++ login.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 22 jun 2023 om 11:36
+-- Serverversie: 10.4.28-MariaDB
+-- PHP-versie: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `username`
+--
+-- Fout bij lezen van tabelstructuur van tabel login.username: #1932 - Table &#039;login.username&#039; doesn&#039;t exist in engine
+-- Fout bij het lezen van gegevens voor tabel login.username: #1064 - Er is iets fout in de gebruikte syntax bij &#039;FROM `login`.`username`&#039; in regel 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
